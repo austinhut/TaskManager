2bdb512940dfe64c0855b0b1af9d02db74b3763d
Austin Hutmacher <hutmacheraustin@gmail.com>
2023-11-25 20:29:20 -0700

    task manager repo

---
+++ app.js
@@ -0,0 +1,17 @@
+const TestApplication = require('./logger');
+const fs = require('fs');
+const testapplication = new TestApplication();
+let data = 'application loaded!\n';
+
+testapplication.on('loadApplication', ()=>{
+  fs.appendFile('logger.txt', data, (err)=>{
+    if (err){
+      throw error;
+    }
+    else {
+      console.log('finished!');
+    }
+  })
+});
+
+testapplication.loadApplication('application is loading...');
